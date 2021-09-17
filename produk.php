55bd5fe11eda3635c61a33d1635f2d181c2f96f1
193040126 <193040126.diva@mail.unpas.ac.id>
2021-09-17 20:36:00 +0700

    menyelesaikan video OOP dasar pada PHP dengan topik property dan method

--- produk.php
+++ produk.php
@@ -1,12 +1,37 @@
 <?php
 
 class Produk {
-    public $judul, $penulis, $penerbit, $harga;
+    public $judul = "judul", 
+    $penulis = "penulis", 
+    $penerbit = "penerbit", 
+    $harga = 0;
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
-$produk2 = new Produk();
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Sasuke";
+// var_dump($produk1);
+// $produk2 = new Produk();
 
+// var_dump($produk2);
+$produk3 = new Produk();
+$produk3->judul = "Sasuke";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Run";
+$produk3->harga = 50000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
 ?>
