16a22f88862bff016c62d276e0cbfe61f93cc7ae
193040126 <193040126.diva@mail.unpas.ac.id>
2021-09-17 20:17:30 +0700

    menambahkan jumlah produk menjadi total 3 produk dan instansiasi

--- produk.php
+++ produk.php
@@ -1,7 +1,12 @@
 <?php
+
 class Produk {
-public $judul, $penulis, $penerbit, $harga;
+    public $judul, $penulis, $penerbit, $harga;
 }
+
 $produk1 = new Produk();
+$produk2 = new Produk();
+$produk2 = new Produk();
 var_dump($produk1);
+
 ?>
