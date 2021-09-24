b222422b4c5ece390b4de525cd3e7d51478677a6
193040126 <193040126.diva@mail.unpas.ac.id>
2021-09-24 15:16:37 +0700

    menambahkan kelas baru yang bernama inheritance.php dan menyelesaikan video OOPHP Dasar tentang inheritance bagian 2

---
+++ inheritance.php
@@ -0,0 +1,79 @@
+<?php
+
+class Produk {
+    public $judul, 
+    $penulis, 
+    $penerbit, 
+    $harga,
+    $jmlHalaman,
+    $waktuMain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
+    $harga = 0, $jmlHalaman = 0, $waktuMain = 0){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk(){
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga
+            })";
+
+        return $str;
+    }
+
+}
+
+class Komik extends Produk {
+    public function getInfoProduk(){
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga
+        }) - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk{
+    public function getInfoProduk(){
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga
+        }) ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk{
+    public function cetak( Produk $produk ){
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Sasuke";
+// var_dump($produk1);
+// $produk2 = new Produk();
+
+// var_dump($produk2);
+$produk1 = new Komik("Sasuke", "Masashi Kishimoto", "Shonen Run", 50000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+
+// echo "Komik : " . $produk1->getLabel();
+// echo "<br>";
+// echo "Game : " . $produk2->getLabel();
+
+// echo "<br>";
+// $infoProduk1 = new CetakInfoProduk();
+// echo $infoProduk1->cetak($produk1);
+
+?>
